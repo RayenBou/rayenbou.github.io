4bf3d2ea0cb3a283dc763065f240c375c771c5cb
Boumaza Rayen <boumaza.rayen@outlook.fr>
2024-07-10 20:00:51 +0200

    Updates

--- assets/js/c8df2e0f.731b3d49.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[101],{8896:t=>{t.exports=JSON.parse('{"tag":{"label":"intro","permalink":"/blog/tags/intro","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/intro","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
