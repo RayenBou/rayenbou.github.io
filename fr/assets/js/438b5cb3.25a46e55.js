f97cbf1aa278f777792d56e0776311c8f5eb1843
Boumaza Rayen <boumaza.rayen@outlook.fr>
2024-07-19 17:08:12 +0200

    Updates

---
+++ fr/assets/js/438b5cb3.25a46e55.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[575],{7041:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/fr/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
